d5c6271719d459ba3800c006996a19aa6f96106f
AlexGeniusMan <alexgeniusman@gmail.com>
2020-11-05 22:08:31 +0300

    added parts: 17, 18

---
+++ src/ru/alexgeniusman/java2020/part_17/Main.java
@@ -0,0 +1,70 @@
+package ru.alexgeniusman.java2020.part_17;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void print(){
+        // READ
+        try (FileReader fileReader = new FileReader("exFile.txt")) {
+            System.out.println("New text from the file:");
+            Scanner scan = new Scanner(fileReader);
+            while (scan.hasNextLine()) {
+                System.out.println(scan.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.getMessage();
+        } catch (IOException e) {
+            e.getMessage();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // POST
+        try (FileWriter fileWriter_1 = new FileWriter("exFile.txt", false)) {
+            System.out.println("Enter the text that will be added to the file:");
+            Scanner in = new Scanner(System.in);
+            String str = in.nextLine();
+            fileWriter_1.write(str);
+            fileWriter_1.close();
+        } catch (IOException e) {
+            e.getMessage();
+        }
+
+        print();
+
+        // PUT
+        try (FileWriter fileWriter_2 = new FileWriter("exFile.txt", false)) {
+            System.out.println("Enter the text that will replace text in the file:");
+            Scanner in = new Scanner(System.in);
+            String str = in.nextLine();
+            fileWriter_2.write(str);
+            fileWriter_2.close();
+
+        } catch (IOException e) {
+            e.getMessage();
+        }
+
+        print();
+
+        // PATCH
+        try (FileWriter fileWriter_3 = new FileWriter("exFile.txt", true)) {
+            System.out.println("Enter the text that will be added to the text in the file:");
+            Scanner in = new Scanner(System.in);
+            String str = in.nextLine();
+            fileWriter_3.write(str);
+            fileWriter_3.close();
+
+        } catch (IOException e) {
+            e.getMessage();
+        }
+
+        print();
+    }
+}
+
---
+++ src/ru/alexgeniusman/java2020/part_18/Main.java
@@ -0,0 +1,30 @@
+package ru.alexgeniusman.java2020.part_18;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public void exceptionDemo() {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Enter an integer");
+        String intString = myScanner.next();
+        int i = Integer.parseInt(intString);
+        try {
+            if (2.0/i==Float.POSITIVE_INFINITY) {
+                System.out.println("INF");
+            }
+            else {
+                System.out.println(2.0/i);
+            }
+        } catch (Exception e) {
+            e.getMessage();
+            System.out.println("DIVISION BY ZERO");
+        }
+
+
+    }
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.exceptionDemo();
+    }
+}
